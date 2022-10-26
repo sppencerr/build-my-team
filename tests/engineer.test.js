36c9e37e28b62281a8e340c8eb52ef6cbf6a2287
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-10-26 02:17:24 -0600

    manager, engineer

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -2,44 +2,44 @@ const Engineer = require("../lib/engineer");
 
 describe("Engineer", ()=>{
     describe("Initialization", ()=>{
-        it("should return an object containing a name, id, email and github property when called with the 'new' keyword and values supplies for 'name, id, email, github'", ()=>{
-            const eng = new Engineer("ENG", "james", "1234", "james@gmail.com", "conradj");
+        it("should return an object containing a name, id, email and github property when called with the 'new' keyword also should return 'name, id, email, github'", ()=>{
+            const eng = new Engineer("ENG", "kessia", "1234", "kessia@email.com", "kessington");
 
-            expect(eng.name).toBe("james");
+            expect(eng.name).toBe("kessia");
             expect(eng.id).toBe("1234");
-            expect(eng.email).toBe("james@gmail.com");
-            expect(eng.github).toBe("conradj")
+            expect(eng.email).toBe("kessia@email.com");
+            expect(eng.github).toBe("kessington")
 
         });
     });
     
     describe("Engineer getName", ()=>{
         it("should return engineer name", ()=>{
-            const engName = new Engineer("ENG", "jimmy", "4321", "gerry@gerrymail.com", "jefer").getName();
-            expect(engName).toBe("jimmy");
+            const engName = new Engineer("ENG", "adam", "4321", "adam@atom.com", "microatom").getName();
+            expect(engName).toBe("adam");
         });
     });
     describe("Engineer getId", ()=>{
         it("should return engineer id", ()=>{
-            const engId = new Engineer("ENG", "jimmy", "12132", "gerry@gerrymail.com", "jefer").getId();
+            const engId = new Engineer("ENG", "adam", "12132", "adam@atom.com", "microatom").getId();
             expect(engId).toBe("12132");
         });
     });
     describe("Engineer getEmail", ()=>{
         it("should return engineer's email", ()=>{
-            const engEmail = new Engineer("ENG", "jimmy", "12132", "gerry!@gerrymail.com", "jefer").getEmail();
-            expect(engEmail).toBe("gerry!@gerrymail.com");
+            const engEmail = new Engineer("ENG", "adam", "12132", "adam!@atom.com", "microatom").getEmail();
+            expect(engEmail).toBe("adam!@atom.com");
         });
     });
     describe("Engineer getGithub", ()=>{
         it("should return engineer's github account", ()=>{
-            const engGithub = new Engineer("ENG", "jimmy", "12132", "gerry!@gerrymail.com", "jefer").getGithub();
-            expect(engGithub).toBe("jefer");
+            const engGithub = new Engineer("ENG", "adam", "12132", "adam!@atom.com", "microatom").getGithub();
+            expect(engGithub).toBe("microatom");
         });
     });
     describe("Engineer getRole", ()=>{
         it("should return 'Engineer'", ()=>{
-            const engRole = new Engineer("ENG", "jimmy", "12132", "gerry!@gerrymail.com", "jefer").getRole();
+            const engRole = new Engineer("ENG", "adam", "12132", "adam!@atom.com", "microatom").getRole();
             expect(engRole).toBe("Engineer");
         });
     });
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,46 @@
-const Employee = require("../")
+const Manager = require("../lib/manager");
+
+describe("Manager", ()=>{
+    describe("Initialization", ()=>{
+        it("should return an object containing a name, id, email and office num property when called with the 'new' keyword it should also return 'name, id, email, office number'", ()=>{
+            const mgr = new Manager("MGR", "kessia", "1234", "kessia@email.com", "1");
+
+            expect(mgr.name).toBe("kessia");
+            expect(mgr.id).toBe("1234");
+            expect(mgr.email).toBe("kessia@email.com");
+            expect(mgr.office).toBe("1");
+
+        });
+    });
+    
+    describe("Manager getName", ()=>{
+        it("should return Manager name", ()=>{
+            const mgrName = new Manager("MGR", "kessManager", "320413", "mnger@coldmail.com", "1").getName();
+            expect(mgrName).toBe("kessManager");
+        });
+    });
+    describe("Manager getId", ()=>{
+        it("should return manager id", ()=>{
+            const mgrId = new Manager("MGR", "kessManager", "320413", "mnger@coldmail.com", "1").getId();
+            expect(mgrId).toBe("320413");
+        });
+    });
+    describe("Manager getEmail", ()=>{
+        it("should return an managers's email", ()=>{
+            const mgrEmail = new Manager("MGR", "kessManager", "320413", "mnger@coldmail.com", "1").getEmail();
+            expect(mgrEmail).toBe("mnger@coldmaill.com");
+        });
+    });
+    describe("Manager getOffice", ()=>{
+        it("should return an managers's office number", ()=>{
+            const mgrOffice = new Manager("MGR", "kessManager", "320413", "mnger@coldmail.com", "1").getOffice();
+            expect(mgrOffice).toBe("1");
+        });
+    });
+    describe("Manager getRole", ()=>{
+        it("should return 'Manager'", ()=>{
+            const mgrRole = new Manager("MGR", "kessManager", "320413", "mnger@coldmail.com", "1").getRole();
+            expect(mgrRole).toBe("Manager");
+        });
+    });
+});
