2a9c98b0d2222b98200a22e374a23a01a6a26c50
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-10-26 01:58:40 -0600

    name, eng.test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,6 +1,46 @@
 const Engineer = require("../lib/engineer");
 
-TextDecoderStream("Can instantiate Engineer instance", () => {
-    const e = Employee();
-    expect(behavior(of)FormDataEvent;)
-})
+describe("Engineer", ()=>{
+    describe("Initialization", ()=>{
+        it("should return an object containing a name, id, email and github property when called with the 'new' keyword and values supplies for 'name, id, email, github'", ()=>{
+            const eng = new Engineer("ENG", "james", "1234", "james@gmail.com", "conradj");
+
+            expect(eng.name).toBe("james");
+            expect(eng.id).toBe("1234");
+            expect(eng.email).toBe("james@gmail.com");
+            expect(eng.github).toBe("conradj")
+
+        });
+    });
+    
+    describe("Engineer getName", ()=>{
+        it("should return engineer name", ()=>{
+            const engName = new Engineer("ENG", "jimmy", "4321", "gerry@gerrymail.com", "jefer").getName();
+            expect(engName).toBe("jimmy");
+        });
+    });
+    describe("Engineer getId", ()=>{
+        it("should return engineer id", ()=>{
+            const engId = new Engineer("ENG", "jimmy", "12132", "gerry@gerrymail.com", "jefer").getId();
+            expect(engId).toBe("12132");
+        });
+    });
+    describe("Engineer getEmail", ()=>{
+        it("should return engineer's email", ()=>{
+            const engEmail = new Engineer("ENG", "jimmy", "12132", "gerry!@gerrymail.com", "jefer").getEmail();
+            expect(engEmail).toBe("gerry!@gerrymail.com");
+        });
+    });
+    describe("Engineer getGithub", ()=>{
+        it("should return engineer's github account", ()=>{
+            const engGithub = new Engineer("ENG", "jimmy", "12132", "gerry!@gerrymail.com", "jefer").getGithub();
+            expect(engGithub).toBe("jefer");
+        });
+    });
+    describe("Engineer getRole", ()=>{
+        it("should return 'Engineer'", ()=>{
+            const engRole = new Engineer("ENG", "jimmy", "12132", "gerry!@gerrymail.com", "jefer").getRole();
+            expect(engRole).toBe("Engineer");
+        });
+    });
+});
