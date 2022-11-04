1c08a6f3a9a1612c84de48d79db911527ac8d38f
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-11-04 03:31:03 -0600

    models instead

--- index.js
+++ index.js
@@ -1,21 +1,147 @@
-const { htmlGenerator } = require('./src/temp');
-const { writeFile } = require('./src/temp');
-const { getAnswers } = require('./src/temp');
+const Employee = require("./models/employeeModels");
+const Manager = require("./models/employeeModels");
+const Intern = require("./models/employeeModels");
+const Engineer = require("./models/employeeModels");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const render = require("./src/pageTemplate")
 
-
-const init = async () => {
-    
-   
-    const answers = await getAnswers("Mngr");
-    
-    
-    const htmlString = await htmlGenerator(answers);
-    
-   
-    const fileWritten = await writeFile(htmlString, "builtTeam.html");
-    
-    // ture or false confirmation to end
-    return fileWritten;
+const employeeIds = []
+const teamMembers = []
+function mainMenu() {
+    function newManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Please enter a manager's name",
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Please enter the manager's ID number"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please enter the manager's email"
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What is the manager's office number?"
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
+            teamMembers.push(manager);
+            employeeIds.push(answers.managerId)
+            buildTeam();
+        })
+    }
+    newManager();
+}
+mainMenu();
+function buildTeam() {
+    inquirer.prompt([
+        {type:"list",
+        name: "addAnEmployee",
+        message: "Which type of employee would you like to add to your team?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "New manager",
+            "None"
+        ]
+        }
+    ]).then(choice => {
+        if (choice.addAnEmployee == "Engineer") {
+            addEngineer();
+        } else if (choice.addAnEmployee == "Intern"){
+            addIntern();
+        } else if (choice.addAnEmployee == "New Manager") {
+            newManager();
+        } else {
+            console.log("Thank you for using my app! Generating your page now...");
+            createPage();
+        }
+    })
 }
 
-init();
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "Please enter the engineer's name",
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "Please enter the engineer's ID number"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "Please enter the engineer's email"
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's Github?"
+        }]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer)
+            endOfAddAnEmployee();
+        })
+}
+function endOfAddAnEmployee () {
+    inquirer.prompt([
+        {
+            type: "list",
+            name:"yesOrNo",
+            message: "Would you like to add another team member?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        }
+    ]).then(choice => {
+        if (choice.yesOrNo == "Yes") {
+            buildTeam();
+        } else {
+            console.log("Thank you for using my app! Generating your page now...");
+            createPage();
+        }
+    })
+}
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "Please enter the intern's name",
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "Please enter the intern's ID number"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "Please enter the intern's email"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Which school does the intern attend?"
+        }]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamMembers.push(intern)
+            endOfAddAnEmployee();
+        })
+}
+
+function createPage() {
+    fs.writeFileSync("page.html",render(teamMembers), "utf-8")
+}
--- src/temp.js
+++ src/temp.js
@@ -1,264 +1,87 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const Manager = require("../lib/manager");
-const engineer = require("../lib/engineer");
-const Intern = require("../lib/intern");
+const wholeTeam = team => {
 
-// build questions for manager
-const questionsMngr = [
-  {
-    type: "input",
-    message: "Team Manager Name: ",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "Employee ID: ",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "Employee Email: ",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "Manager Office Number: ",
-    name: "office",
-  },
-];
+  // create the manager html
+  const buildManagerCard = manager => {
+      return `
+  <div>
+      <div>
+          <h2>${manager.getName()}:${manager.getRole()} </h2>
+      </div>
+      <div>
+          <h4> ID: ${manager.getId()}</h4>
+          <h4> Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></h4>
+          <h4> Office number: ${manager.getOfficeNumber()}</h4>
+      </div>
+  </div>
+      `;
+  };
 
-// build questions for engineer
-const questionsEng = [
-  {
-    type: "input",
-    message: "engineer Name: ",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "Employee ID: ",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "Employee Email: ",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "Github Username: ",
-    name: "gituser",
-  },
-];
+  const buildEngineerCard = engineer => {
+      return `
+  <div>
+      <div>
+          <h2>${engineer.getName()}: ${engineer.getRole()}</h2>
+      </div>
+  <div>
+      <h4>ID: ${engineer.getId()}</h4>
+      <h4>Email: ${engineer.getEmail()}</h4>
+      <h4>GitHub: ${engineer.getGithub()}</h4>
+  </div>
+  </div>
+      `;
+  };
 
-// build questions for Intern
-const questionsInt = [
-  {
-    type: "input",
-    message: "Intern Name: ",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "Employee ID: ",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "Employee Email: ",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "Intern School: ",
-    name: "school",
-  },
-];
+  const buildIntenCard = intern => {
+      return `
+  <div>
+      <div>
+      <h2>${intern.getName()}:${intern.getRole()}</h2>
+      </div>
+      <div>
+          <h4>ID: ${intern.getId()}</h4>
+          <h4>Email: ${intern.getEmail()}</h4>
+          <h4>School: ${intern.getSchool()}</h4>
+      </div>
+  </div>
+      `;
+  };
 
-// questions to continue or not
-const questionsCont = {
-  type: "list",
-  name: "nextemp",
-  message: "Add another employee?",
-  choices: [
-    { value: "eng", name: "engineer" },
-    { value: "Int", name: "Intern" },
-    { value: "done", name: "Done" },
-  ],
-};
+  const html = [];
 
-questionsMngr.push(questionsCont);
-questionsEng.push(questionsCont);
-questionsInt.push(questionsCont);
-
-function Employee(type, name, id, email) {
-  this.type = type;
-  this.name = name;
-  this.id = id;
-  this.email = email;
-}
-
-const getAnswers = async (employeeType, employees = []) => {
-  let questions = [];
-  let answers = [];
-  switch (employeeType) {
-    case "Mngr":
-      questions = questionsMngr;
-      break;
-    case "Eng":
-      questions = questionsEng;
-      break;
-    case "Int":
-      questions = questionsInt;
-      break;
-    default:
-      done = true;
-      break;
-  }
-  answers = await inquirer.prompt(questions);
-
-  answers.type = employeeType;
-
-  employees.push(answers);
-
-  if (answers.nextemp !== "done") {
-    return getAnswers(answers.nextemp, employees);
-  } else {
-    return employees;
-  }
-};
-
-const htmlGenerator = async (data) => {
-  let employees = [];
-  for (let i = 0; i < data.length; i++) {
-    employees[i] = new Employee(
-      data[i].type,
-      data[i].name,
-      data[i].id,
-      data[i].email
-    );
-    switch (data[i].type) {
-      case "Mngr":
-        employees[i] = new Manager(
-          data[i].type,
-          data[i].name,
-          data[i].id,
-          data[i].email,
-          data[i].office
-        );
-        break;
-      case "Eng":
-        employees[i] = new engineer(
-          data[i].type,
-          data[i].name,
-          data[i].id,
-          data[i].email,
-          data[i].gituser
-        );
-        break;
-      case "Int":
-        employees[i] = new Intern(
-          data[i].type,
-          data[i].name,
-          data[i].id,
-          data[i].email,
-          data[i].school
-        );
-        break;
-      default:
-        break;
-    }
-  }
-
-  function getCardHeader(type, name) {
-    let returnString = "";
-    switch (type) {
-      case "Mngr":
-        returnString = `<img class="" src="img/coffe.png" style="max-height:30px;" alt="sippin"/> <span class="font-weight-bold h5" >Manager</span>`;
-        break;
-      case "Eng":
-        returnString = `<img class="" src="img/glasses.png" style="max-height:30px; " alt="bozo"/> <span class="font-weight-bold h5" >Engineer</span>`;
-        break;
-      case "Int":
-        returnString = `<img class="" src="img/student.png" style="max-height:30px; " alt="fresh student"/> <span class="font-weight-bold h5" >Intern</span>`;
-        break;
-    }
-    returnString += `<h4 class="card-title">${name}</h4>`;
-    return returnString;
-  }
-
-  function getCardFooter(type, obj) {
-    let returnString = "";
-    switch (type) {
-      case "Mngr":
-        returnString = `<p class="card-text">Office: <span class="font-weight-bold">${obj.office}</span></p>`;
-        break;
-      case "eng":
-        returnString = `<p class="card-text">Github: <span class="font-weight-bold"> <a href="https://github.com/${obj.github}" target="_blank">${obj.github}</a></span></p>`;
-        break;
-      case "Int":
-        returnString = `<p class="card-text">School: <span class="font-weight-bold">${obj.school}</span></p>`;
-        break;
-    }
-    return returnString;
-  }
-
-  let htmlString = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Your Team Profile</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
-    </head>
-    <body>
-        <main class="d-flex flex-wrap">
-        <section class="jumbotron d-flex flex-column col-12 text-center" style="color:#080808; background-image: linear-gradient(to right, rgb(252, 3, 140),rgb(245, 198, 224) );">
-            <h1 class="title">Team Members</h1>
-        </section>
-        <section class="mx-auto row" style="">`;
-
-  for (let i = 0; i < employees.length; i++) {
-    htmlString += `<div class="card col-lg-2 col-md-6 col-sm-6 m-2 p-0" style="min-width:300px;">
-        <div class="card-body border border-info">
-            <div class="mb-2">
-                ${getCardHeader(employees[i].type, employees[i].name)}
-            </div>
-            <p class="card-text">Employee Id: <span class="font-weight-bold"> ${
-              employees[i].id
-            }</span></p>
-            <p class="card-text">Email: <span class="font-weight-bold"> <a href="mailto:${
-              employees[i].email
-            }" target="_blank">${employees[i].email}</a></span></p>
-            ${getCardFooter(employees[i].type, employees[i])}
-        </div>
-    </div>`;
-  }
-
-  return htmlString;
-};
-
-//store string, generate document
-const writeFile = async (data, filename) => {
-  let fileDir = "dist/";
-  fileName = fileDir + filename;
-
-  fs.appendFile(fileName, data, (err) =>
-    err
-      ? console.error(err)
-      : console.log(
-          "\x0A\x0A The team profile @ builtTeam.html is ready go to /dist to check..\x0A\x0A"
-        )
+  html.push(team
+      .filter(employee => employee.getRole() === "Manager")
+      .map(manager => buildManagerCard(manager))
+  );
+  html.push(team
+      .filter(employee => employee.getRole() === "Engineer")
+      .map(engineer => buildEngineerCard(engineer))
+      .join("")
+  );
+  html.push(team
+      .filter(employee => employee.getRole() === "Intern")
+      .map(intern => buildIntenCard(intern))
+      .join("")
   );
 
-  return true;
-};
+  return html.join("");
 
-module.exports = {
-  htmlGenerator,
-  writeFile,
-  getAnswers,
+}
+
+// exports the actual page
+module.exports = team => {
+
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Team Profile Generator</title>
+</head>
+<body>
+  ${wholeTeam(team)}
+</body>
+</html>
+  `;
 };
